00749a30419395af2cd4df625cc40b2a2338a112
Erin Turnbull <erin.a.turnbull@gmail.com>
2022-06-09 16:52:18 -0400

    Added guards for descriptor loading so that we don't require all the modules

--- src/universalio/__init__.py
+++ src/universalio/__init__.py
@@ -9,11 +9,14 @@ from .fileman import _FileWrapper
 @_injector.inject
 def _setup_file_manager(f: FileManager):
     import universalio.descriptors as desc
+    # Check local file system first
     f.register_descriptor_class(desc.LocalDescriptor, 0)
+    # Check HTTP structures
     if hasattr(desc, "SFTPDescriptor"):
         f.register_descriptor_class(desc.SFTPDescriptor, 100)
     if hasattr(desc, "AzureBlobDescriptor"):
         f.register_descriptor_class(desc.AzureBlobDescriptor, 100)
+    # Fallback for HTTP
     if hasattr(desc, "HttpDescriptor"):
         f.register_descriptor_class(desc.HttpDescriptor, 10000)
 
