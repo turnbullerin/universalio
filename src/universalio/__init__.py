506a46a794abd1b912977e807ec6638d0bbeb4cb
Erin Turnbull <erin.a.turnbull@gmail.com>
2022-06-14 17:22:25 -0400

    Adopted new procedures in zirconium for loading configuration

--- src/universalio/__init__.py
+++ src/universalio/__init__.py
@@ -1,26 +1,14 @@
-from importlib.util import find_spec as _find_spec
-from autoinject import injector as _injector
-
 from .global_loop import GlobalLoopContext
 from .fileman import FileManager
 from .fileman import _FileWrapper
+import zirconium
 
 
-@_injector.inject
-def _setup_file_manager(f: FileManager):
-    import universalio.descriptors as desc
-    # Check local file system first
-    f.register_descriptor_class(desc.LocalDescriptor, 0)
-    # Check HTTP structures
-    if hasattr(desc, "SFTPDescriptor"):
-        f.register_descriptor_class(desc.SFTPDescriptor, 100)
-    if hasattr(desc, "AzureBlobDescriptor"):
-        f.register_descriptor_class(desc.AzureBlobDescriptor, 100)
-    # Fallback for HTTP
-    if hasattr(desc, "HttpDescriptor"):
-        f.register_descriptor_class(desc.HttpDescriptor, 10000)
+@zirconium.configure
+def _config_file_manager(config):
+    # Default configuration paths
+    config.register_file("~/.universalio.toml", weight=-5)
+    config.register_file("./.universalio.toml", weight=-4)
 
-
-_setup_file_manager()
 
 FileWrapper = _FileWrapper()
