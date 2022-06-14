931deb937f4e855db61d82ac355fdfa7d159f288
Erin Turnbull <erin.a.turnbull@gmail.com>
2022-06-14 17:21:55 -0400

    Added support for loading descriptors from an entry point

--- src/universalio/fileman.py
+++ src/universalio/fileman.py
@@ -1,18 +1,47 @@
 from autoinject import injector
+from universalio.descriptors.base import ResourceDescriptor
+import universalio.descriptors as desc
+
+# Backwards support for Python 3.7
+import importlib.util
+if importlib.util.find_spec("importlib.metadata"):
+    from importlib.metadata import entry_points
+else:
+    from importlib_metadata import entry_points
 
 
 @injector.injectable
 class FileManager:
 
-    def __init__(self):
+    def __init__(self, include_entry_points=True):
         self.registry = {}
         self.registry_order = None
+        # Check local file system first
+        self.register_descriptor_class(desc.LocalDescriptor, 0)
+        # Todo, move these to their own module eventually
+        if hasattr(desc, "SFTPDescriptor"):
+            self.register_descriptor_class(desc.SFTPDescriptor, 100)
+        if hasattr(desc, "AzureBlobDescriptor"):
+            self.register_descriptor_class(desc.AzureBlobDescriptor, 100)
+        if include_entry_points:
+            auto_register = entry_points(group="universalio.descriptors")
+            for ep in auto_register:
+                cls = ep.load()
+                weight = 100
+                if hasattr(cls, "weight"):
+                    weight = getattr(cls, "weight")
+                self.register_descriptor_class(cls, weight)
+        # Fallback for HTTP
+        if hasattr(desc, "HttpDescriptor"):
+            self.register_descriptor_class(desc.HttpDescriptor, 10000)
 
     def register_descriptor_class(self, descriptor_class: type, weight: int=0):
         self.registry[descriptor_class.__name__] = (descriptor_class, weight)
         self.registry_order = None
 
-    def get_descriptor(self, location):
+    def get_descriptor(self, location) -> ResourceDescriptor:
+        if isinstance(location, ResourceDescriptor):
+            return location
         if self.registry_order is None:
             sort_me = [(key, self.registry[key][1]) for key in self.registry]
             sort_me.sort(key=lambda x: x[1])
@@ -31,5 +60,5 @@ class _FileWrapper:
     def __init__(self):
         pass
 
-    def __call__(self, location):
+    def __call__(self, location) -> ResourceDescriptor:
         return self.file_manager.get_descriptor(location)
